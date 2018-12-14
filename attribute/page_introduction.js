62e0ab4deb75a0e1d4c9d0668603eb4e59797e4b
hzy <69162582@qq.com>
2018-12-14 21:35:31 +0800

    1214

---
+++ attribute/page_introduction.js
@@ -0,0 +1,16 @@
+page_introduction = `
+<div style="width: 1000px; margin: auto auto; font-size: 24px;">
+<br><br>
+亲爱的同学：
+<br><br>
+&emsp;&emsp;你好，欢迎参加本次研究。您的测试信息和结果将被保密，仅用于科研分析，请放心。
+<br><br>
+整个测试流程不超过15min
+<br><br>
+全程请保持放松舒适的状态，按照指示操作即可
+<br><br>
+期间如果有任何疑问，请及时通知主试
+<br><br>
+如果你已经准备好了，请按“空格键”开始本次测试 (*^▽^*)
+</div>
+`;
---
+++ attribute/page_tea_menu.js
@@ -0,0 +1,33 @@
+page_tea_menu =` 
+<div style="width: 100%; height: 100%; padding: 0; margin: 0; font-size: 24px; text-align: justify;">
+	<div style="width: 100%; padding: 50px 0; margin: 0;  text-align: center;">
+		<img src="../resource/御茗阁2.png">
+		<img src="../resource/cup.png">
+	</div>
+	<div style="line-height: 1.5; width:100%; margin: 0; text-align: center;">
+		<p>尊敬的顾客，欢迎来到御茗阁！<br>我们为您提供天然、绿色、健康、安全的高品质茶叶<br>请放心选择^_^</p>
+		<p>请点击下方的菜单浏览茶叶目录，选择您喜欢的茶叶！</p>
+	</div>
+	<div style="width: 100%; margin: 0; padding: 0; text-align: center;">
+		<ul id="main_menu" style="width: 634px; height: 133px; margin: 0; padding: 0; display: inline-block; list-style: none;">
+			<li class="i1" id="green_tea_main">
+				<a href="#" class="a1">
+				</a>
+			</li>
+			<li class="i2" id="red_tea_main">
+				<a href="#" class="a2">
+				</a>
+			</li>
+			<li class="i2" id="qing_tea_main">
+				<a href="#" class="a3">
+				</a>
+			</li>
+			<li class="i1" id="hua_tea_main">
+				<a href="#" class="a4">
+				</a>
+			</li>
+		</ul>
+	</div>
+
+</div>
+`;
