9f5fcfbec59a7a07dd92c3b9a76f0daeeec37c6e
Cao <337287307@qq.com>
2018-12-15 21:53:55 +0800

    支持多个物品

---
+++ attribute/page_shampoo_introduction.js
@@ -0,0 +1,26 @@
+page_shampoo_introduction = `
+<div style="width: 1000px; height: 100%; padding: 0; margin: 0 auto; font-size: 18px; line-height:30px; text-align: justify; /*background-color: red*/;">
+	<div>
+		<br>
+		<p>这个是shampoo instruction！：</p>
+		<p>&emsp;&emsp;茶叶源于中国，距今已有6000多年的历史。茶叶中所含的有机化合物在450种以上，其中儿茶素、胆甾烯酮、咖啡碱、肌醇、叶酸、泛酸等成分含量较高，经过现在生物化学和医学的研究，充分证明茶叶既有营养价值，又有药理作用，如提神健脑、降脂减肥、预防心脑血管疾病等，与人们的健康关系十分密切。
+		</p>
+		<p>&emsp;&emsp;茶叶的分类方式有很多，依据季节采制可分为春茶、夏茶、秋茶、冬茶。按照制茶的火焙程度可以分为绿茶、红茶、青茶、白茶、黄茶、黑茶。除此之外，还有以各种毛茶或精制茶再加工而成的再加茶，如花茶、紧压茶、萃取茶等。
+		</p>
+		<p>以下是几种常见茶叶的简单介绍：</p>
+		<p>
+		&emsp;&emsp;绿茶：不发酵茶（发酵度为零），清汤绿叶
+		<br>&emsp;&emsp;黄茶：微发酵茶（发酵度为10～20m），黄汤黄叶
+		<br>&emsp;&emsp;白茶：轻发酵茶（发酵度为20～30m），汤色晶亮，鲜爽带甜
+		<br>&emsp;&emsp;青茶：又称乌龙茶，属半发酵茶，绿叶红镶边
+		<br>&emsp;&emsp;红茶：全发酵茶（发酵度为80～90m），红汤红叶
+		<br>&emsp;&emsp;黑茶：后发酵茶（发酵度为100m），橙黄汤色，醇和滋味
+		<br>&emsp;&emsp;花茶：又名香片，属再加工茶。它是用花香增加茶香的一种产品，根据茶叶容易吸收异味的特点，以香花窨料加工而成
+		</p>
+	</div>
+	<div id="next_tips" style="text-align:right;display:none;">
+		<br>
+		<p>如阅读完毕，请按“N”进入下一页</p>
+	</div>
+</div>
+`;
