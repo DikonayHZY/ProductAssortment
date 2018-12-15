0688465c97356c9e5ff10627d848c11b6b64322e
hzy <69162582@qq.com>
2018-12-15 18:18:05 +0800

    新增了洗发水的html和js

---
+++ attribute/page_shampoo_bsl.js
@@ -0,0 +1,92 @@
+page_shampoo_bsl =`
+	<div style="width: 100%; height: 100%; position: fixed;">
+		<div style="width: 100%; height: 20%;"><!-- 顶部 -->
+			<div style="width: 100%; height: 50%; min-height: 40px; background-color: black; display: flex; align-items: center; justify-content: center;">
+				<img src="../resource/shampoo_logo2.png">
+			</div>
+			<div style="width: 100%; height: 50%; background-color: #262626; display: flex; align-items: center; justify-content: center; font-size: 26px; color: white;">白松露系列</div>
+		</div>
+
+		<div style="width: 100%; height: 80%; display: flex;"> <!-- 下半部分 -->
+			<div style="width: 15%; height: 100%; min-width: 150px; /*background-color: green; */display: flex; align-items: center; justify-content: center;"> <!-- 左侧导航栏 -->
+				<div class="shampoo_left_menu" style="width: 100%; height: 70%; display: flex; flex-direction: column; align-items: center; justify-content: center; ">
+					<a href="#" id="left_green_tea">白松露</a><br>
+					&emsp;<br>
+					<a href="#" id="left_red_tea">鱼子酱</a><br>
+					&emsp;<br>
+					<a href="#" id="left_qing_tea">牛油果</a><br>
+					&emsp;<br>
+					<a href="#" id="left_hua_tea">北欧木</a>
+				</div>
+			</div>
+
+			<div style="width: 85%; height: 100%;"> <!-- 正文部分 -->
+				<div style="width: 100%; height: 45%; /*background-color: green;*/ display: flex; justify-content: center;"> <!-- 商品陈列区 -->
+					<div class="shampoo_chenlie" style="width: 80%; height: 100%; display: flex; justify-content: space-around;">					
+						<a href="#" style="width: 15%; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;" id="choice_1">
+							<img id="pic_1" src="1.png" style="height: 80%;">
+							<p id="text_1">洗发水</p>
+						</a>
+						<a href="#" style="width: 15%; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;" id="choice_2">
+							<img id="pic_2" src="2.png" style="height: 80%;">
+							<p id="text_2">洗发水</p>
+						</a>
+						<a href="#" style="width: 15%; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;" id="choice_3">
+							<img id="pic_3" src="3.png" style="height: 80%;">
+							<p id="text_3">洗发水</p>
+						</a>						
+						<a href="#" style="width: 15%; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;" id="choice_4">
+							<img id="pic_4" src="4.png" style="height: 80%;">
+							<p id="text_4">洗发水</p>
+						</a>
+						<a href="#" style="width: 15%; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;" id="choice_5">
+							<img id="pic_5" src="5.png" style="height: 80%;">
+							<p id="text_5">洗发水</p>
+						</a>						
+					</div>
+				</div>
+
+				<div id="product_discription" style="width: 100%; height: 55%; visibility: hidden;">
+					<div style="height: 20%; width: 100%; display: flex;">
+						<div style="width: 40%; height: 100%; display: flex; flex-direction: column;">
+							<div style="width: 100%; height: 50%; border-bottom: 1px solid #262626;"></div>
+							<div style="width: 100%; height: 50%;"></div>
+						</div>
+						<div  id="tea_name" style="width: 20%; min-width: 200px; height: 100%; display: flex; align-items: center; justify-content: center; font-size: 28px;">
+							选中的洗发水
+						</div>
+						<div style="width: 40%; height: 100%; display: flex; flex-direction: column;">
+							<div style="width: 100%; height: 50%; border-bottom: 1px solid #262626;"></div>
+							<div style="width: 100%; height: 50%;"></div>
+						</div>						
+
+					</div>
+					<div style="height: 80%; width: 100%; display: flex;">
+						<div style=" height: 100%; width: 20%; display: flex; align-items: center; justify-content: center; ">
+							<img  style="height: 90%;" src="" id="select_pic">
+						</div>
+						<div style="height: 100%; width: 55%; display: flex; flex-direction: column; align-items: flex-start; justify-content: center; font-size: 22px; line-height: 2.0;">
+
+							<p><strong>特性成分：</strong><span id="tea_assortment">白松露</span></p>							
+							<p><strong>产品功效：</strong><span id="tea_benefit">祛屑止痒</span></p>
+
+							<p id="other_discription">不含防腐剂，无硅油，不含硫酸盐</p>							
+
+						</div>
+
+						<div style=" height: 100%; width: 25%; display: flex; align-items: center; justify-content: center; ">
+							<a id="buy_button" href="#" style="width: 70%; height: 40px; border: 2px solid black; font-size: 28px; font-weight: 600;visibility: hidden;">选择</a>
+						</div>
+
+					</div>
+					
+				</div>
+
+
+			</div>
+
+		</div>
+
+
+	</div>	
+`;
