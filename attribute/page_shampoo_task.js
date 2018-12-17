28b86cde982b4c65d4b7140d27a6717b0a811280
hzy <69162582@qq.com>
2018-12-18 02:59:30 +0800

    删掉了产品介绍页

--- attribute/page_shampoo_task.js
+++ attribute/page_shampoo_task.js
@@ -1,18 +1,17 @@
-page_shampoo_task = `
-<div style="width: 800px; height: 100%; padding: 0; margin: 0 auto; font-size: 18px; line-height:30px; text-align: justify; /*background-color: red*/;">
-	<div>
-		<br>
-		<p style="font-size: 30px">这个是shampoo情景模拟</p>
-		<p style="font-size: 30px; line-height:40px"><br>&emsp;&emsp;想象一下你正在御茗阁(一家茶馆)喝茶，现在需要你从茶叶目录里选择一款你偏好的茶叶
-		</p>
-		</p>
-		<br>
-		<p>注：御茗阁是全国连锁的品牌茶馆，通过全产业链管控及先进的运营模式控制优质茶叶资源，为顾客提供天然、绿色、健康、安全的高品质茶叶
-		</p>
+page_shampoo_task =`
+	<div style="width: 100%; height: 100%; position: fixed; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+		<div style="width: 70%; height: 25%; display: flex; align-items: center; justify-content: flex-start;"><!-- 任务描述 -->
+			<div style="width: 200px; height: 100%; font-size: 35px; display: flex; justify-content: flex-end;"><strong>情景模拟：</strong></div>
+			<div style="width: 70%; height: 100%; font-size: 26px; line-height: 1.5;">你的洗发水刚好用完了，朋友安利你一个护发品牌ALERNA，你现在打算从ALERNA的洗发水产品里<strong>选择一款你偏好的洗发水</strong></div>
+		</div>
+		<div style="width: 60%; height: 20%; display: flex; align-items: center; justify-content: center; font-size: 18px; line-height: 1.5;">
+			注：ALTERNA一家专注于护发产品的生物科技公司，致力于采用高科技技术，结合创新配方，为顾客提供高品质的护发产品
+		</div>
+
+		<div style=" height: 15%; width: 70%; display: flex; align-items: center; justify-content: center; ">
+			<a id="next_tips" href="#" style="visibility: hidden; width: 250px; height: 50px; display: flex; align-items: center; justify-content: center; border: 2px solid black; font-size: 28px; font-weight: 400; ">进入洗发水目录</a>
+		</div>
+
+
 	</div>
-	<div id="next_tips" style="text-align:right;display:none">
-		<br>
-		<p>请按“N” 进入茶叶目录</p><!-- 15秒后出现这句话 -->
-	</div>
-</div>
 `;
