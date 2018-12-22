432aafc36577d32f18128da93d87fff1fa9f5c10
hzy <69162582@qq.com>
2018-12-22 21:28:51 +0800

    优秀

--- attribute/page_brush_choose_menu.js
+++ attribute/page_brush_choose_menu.js
@@ -13,7 +13,8 @@ page_brush_choose_menu =`
 				<div style="width: 100%; height: 50%;"></div>
 			</div>
 		</div>
-		<div style="width: 100%; height: 80%; display: flex; align-items: center; background:url(../resource/brush2.jpg); background-size: 100% 100%; background-repeat:no-repeat;"> <!-- 下半部分 -->
+		<div style="width: 100%; height: 80%; display: flex; flex-direction: column; justify-content: flex-start; align-items: center; background:url(../resource/brush2.jpg); background-size: 100% 100%; background-repeat:no-repeat;"> <!-- 下半部分 -->
+			<div style="width: 100%; height: 15%; background-color: white; display: flex; align-items: center; justify-content: center; color: black; font-size: 20px;">任务<span id="choose_task_number">1</span>：请找到商品“<span id="answer">P8880_68</span>”，然后点击“确定”按钮</div>
 			<div style="width: 100%; height: 70%; background-color: rgba(235,235,235,0.6); display: flex; flex-direction: column; justify-content: center;">
 				<div style="width: 100%; height: 30%; display: flex; align-items: center; justify-content: center; color: #202020; font-size: 35px;">产品系列</div>
 				<div class="brush_main_menu" style="width: 100%; height: 40%; display: flex; align-items: center; justify-content: center; color: white; font-size: 40px;">
@@ -21,8 +22,7 @@ page_brush_choose_menu =`
 					<a href="#" id="red_tea_main">声波振动型</a>&emsp;<span class="line">&#124;</span>&emsp;
 					<a href="#" id="qing_tea_main">超声波型</a>&emsp;<span class="line">&#124;</span>&emsp;
 					<a href="#" id="hua_tea_main">电动喷雾型</a></div>
-			</div>
-			
+			</div>			
 		</div>
 	</div>	
 `;
