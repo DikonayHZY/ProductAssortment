292e78af7d5487582bbd8e024f051eee9beb495c
hzy <69162582@qq.com>
2018-12-22 21:12:33 +0800

    辣鸡

---
+++ attribute/page_brush_choose_menu.js
@@ -0,0 +1,28 @@
+page_brush_choose_menu =`
+	<div style="width: 100%; height: 100%; position: fixed;">
+		<div style="width: 100%; height: 20%; background-color: #ebebeb; display: flex;"><!-- 顶部 -->
+			<div style="width: 40%; height: 100%; display: flex; flex-direction: column;">
+				<div style="width: 100%; height: 50%; border-bottom: 1px solid #7d7d7d;"></div>
+				<div style="width: 100%; height: 50%;"></div>
+			</div>
+			<div style="width: 20%; min-width: 150px; height: 100%; display: flex; align-items: center; justify-content: center;">
+				<img style="width: 90%;" src="../resource/brush_logo.png">
+			</div>
+			<div style="width: 40%; height: 100%; display: flex; flex-direction: column;">
+				<div style="width: 100%; height: 50%; border-bottom: 1px solid #7d7d7d;"></div>
+				<div style="width: 100%; height: 50%;"></div>
+			</div>
+		</div>
+		<div style="width: 100%; height: 80%; display: flex; align-items: center; background:url(../resource/brush2.jpg); background-size: 100% 100%; background-repeat:no-repeat;"> <!-- 下半部分 -->
+			<div style="width: 100%; height: 70%; background-color: rgba(235,235,235,0.6); display: flex; flex-direction: column; justify-content: center;">
+				<div style="width: 100%; height: 30%; display: flex; align-items: center; justify-content: center; color: #202020; font-size: 35px;">产品系列</div>
+				<div class="brush_main_menu" style="width: 100%; height: 40%; display: flex; align-items: center; justify-content: center; color: white; font-size: 40px;">
+					<a href="#" id="green_tea_main">机械转动型</a>&emsp;<span class="line">&#124;</span>&emsp;
+					<a href="#" id="red_tea_main">声波振动型</a>&emsp;<span class="line">&#124;</span>&emsp;
+					<a href="#" id="qing_tea_main">超声波型</a>&emsp;<span class="line">&#124;</span>&emsp;
+					<a href="#" id="hua_tea_main">电动喷雾型</a></div>
+			</div>
+			
+		</div>
+	</div>	
+`;
