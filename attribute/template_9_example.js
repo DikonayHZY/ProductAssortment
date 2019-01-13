d42c9860f84fde3b14ca9ecd0f5b9680dd31b5f3
hzy <69162582@qq.com>
2019-01-13 19:22:14 +0800

    增加例子

--- attribute/template_9_example.js
+++ attribute/template_9_example.js
@@ -1,18 +1,20 @@
-template_9_dyh =`
+template_9_example =`
 	<div style="width: 100%; height: 100%; position: fixed;">
-		<div style="width: 100%; height: 25%; display: flex; align-items: center; justify-content: center;"><!-- 题目说明 -->
+		<div style="width: 100%; height: 40%; display: flex; align-items: center; justify-content: center;"><!-- 题目说明 -->
 			<div style="width: 80%; height: 100%; display: flex; align-items: center; justify-content: flex-start; font-size: 24px; line-height: 2;"> 
-			<p>请仔细读题，根据题意回忆刚才的感受<br>
-			选择<strong>最能表达你真实感受</strong>的数字：“1”= 非常不符合，“9”= 非常符合<br>
-			<strong style="color: red;">回答不分对错</strong>，请按照真实感受来回答</p>
+			<p><strong>本题与你之前浏览的所有页面均无关联</strong><br>
+			   请仔细阅读下列描述，并<strong>判断事件发生的可能性</strong>：“1”= 非常不可能，“9”= 非常可能<br>
+			   <strong style="color: red;">回答不分对错</strong>，请按照真实感受来回答<br>
+			   <span style="color: blue; font-size: 20px;">请看例题，如果你认为小明非常不可能中奖，那么请选择“1”——非常不可能</span>
+			</p>
 			</div>			
 		</div>
 
-		<div style="width: 100%; height: 55%; display: flex; flex-direction: column; align-items: center; justify-content: center;"><!-- 题目部分 -->
-			<div style="width: 80%; height: 30%; display: flex; align-items: center; justify-content: center; font-size: 26px; line-height: 1.5;" id="content">小张打开电子邮箱后收到垃圾邮件</div>
+		<div style="width: 100%; height: 40%; display: flex; flex-direction: column; align-items: center; justify-content: center;"><!-- 题目部分 -->
+			<div style="width: 80%; height: 40%; display: flex; align-items: center; justify-content: center; font-size: 26px; line-height: 1.5;" id="content">例题：小明买了一张彩票，他中奖的可能性有多大？</div>
 			<div style="width: 100%; height: 50%; display: flex; flex-direction: column; align-items: center; justify-content: space-around;"> 
 				<div style="width: 100%; height: 45%; min-height: 70px; display: flex; align-items: center; justify-content: center;">
-						<div style="width: 70px; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+					<div style="width: 70px; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
 						<div class="TF_radio_box" style="width: 20px; height: 20px;">
 	 						<input name="one_choice" type="radio" id="choice_1" value="1">
 	 						<label for="choice_1"></label>							
@@ -77,17 +79,14 @@ template_9_dyh =`
 					</div>
 				</div>
 				<div style="width: 47%; height: 55%; display: flex; align-items: flex-start; justify-content: space-between;">
-					<p id="left">非常不符合</p>
+					<p id="left">非常不可能</p>
 					<p>中立</p>
-					<p>&ensp;<span id="right">非常符合</span>&ensp;</p>
+					<p>&ensp;<span id="right">非常可能</span>&ensp;</p>
 				</div>
-
-
-
 			</div>
 		</div>
 		<div style="width: 100%; height: 20%; display: flex; align-items: center; justify-content: center;">
-			<a id="next_page" href="#" style="width: 200px; height: 40px; text-align: center; font-size: 26px;/*visibility: hidden*/;">按“空格”翻页</a>
+			<a id="next_page" href="#" style="width: 380px; height: 40px; text-align: center; font-size: 26px;/*visibility: hidden*/;">按“空格”进入正式答题</a>
 		</div>		
 	</div>
 `;
