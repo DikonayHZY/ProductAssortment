cfb2cb97745b14219bc3fb338af4fe38e0b29dbf
hzy <69162582@qq.com>
2019-01-13 21:57:53 +0800

    修改说明页

--- attribute/page_introduction.js
+++ attribute/page_introduction.js
@@ -1,16 +1,14 @@
 page_introduction = `
-<div style="width: 1000px; margin: auto auto; font-size: 24px;">
+<div style="width: 1000px; margin: auto auto; font-size: 24px; line-height: 1.5;">
 <br><br>
 亲爱的同学：
-<br><br>
-&emsp;&emsp;你好，欢迎参加本次研究。您的测试信息和结果将被保密，仅用于科研分析，请放心。
-<br><br>
-整个测试流程约15min左右
-<br><br>
-全程请保持放松舒适的状态，按照指示操作即可
-<br><br>
-请不要按F5或者F11<br><br>
-期间如果有任何疑问，请及时通知主试
+<br>
+&emsp;&emsp;你好，欢迎参加本次研究。本次测试的所有数据仅用于学术研究，任何个人信息不会被报告或公开。感谢您的理解和支持！
+<br>
+&emsp;&emsp;整个测试流程约10min左右，全程请保持放松舒适的状态，按照指示操作即可
+<br>
+&emsp;&emsp;<strong>请不要按F5或者F11</strong><br>
+&emsp;&emsp;期间如果有任何疑问，请及时通知主试
 <br><br>
 如果你已经准备好了，请按“空格键”开始本次测试 (*^▽^*)
 </div>
