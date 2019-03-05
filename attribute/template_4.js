f2edd95a90cd4b59956a34677dc8398ec05b3f5f
hzy <69162582@qq.com>
2019-03-06 04:18:40 +0800

    1

--- attribute/template_4.js
+++ attribute/template_4.js
@@ -1,19 +1,18 @@
 template_4 = `
 	<div style="width: 100%; height: 100%; position: fixed;">
-		<div style="width: 100%; height: 40%; display: flex; align-items: center; justify-content: center;"><!-- 题目说明 -->
+		<div style="width: 100%; height: 25%; display: flex; align-items: center; justify-content: center;"><!-- 题目说明 -->
 			<div style="width: 80%; height: 100%; display: flex; align-items: center; justify-content: flex-start; font-size: 24px; line-height: 2;"> 
-				<p>请仔细阅读题目，并选择<strong>最能表达你真实感受</strong>的数字：“1”= 非常不符合，“9”= 非常符合<br>
-				   <strong style="color: red;">回答不分对错</strong>，请按照真实感受来回答<br>
-				</p>
+			<p>请仔细读题，根据题意回忆刚才的感受<br>
+			选择<strong>最能表达你真实感受</strong>的数字：“1”= 非常不同意，“9”= 非常同意<br>
+			<strong style="color: red;">回答不分对错</strong>，请按照真实感受来回答</p>
 			</div>			
 		</div>
 
-		<div style="width: 100%; height: 40%; display: flex; flex-direction: column; align-items: center; justify-content: center;"><!-- 题目部分 -->
-			<div style="width: 80%; height: 40%; display: flex; align-items: center; justify-content: center; font-size: 26px; line-height: 1.5;" id="content">我感觉完成产品选择的任务需要进行较多的思考
-			</div>
+		<div style="width: 100%; height: 55%; display: flex; flex-direction: column; align-items: center; justify-content: center;"><!-- 题目部分 -->
+			<div style="width: 80%; height: 30%; display: flex; align-items: center; justify-content: center; font-size: 26px; line-height: 1.5;" id="content">小张打开电子邮箱后收到垃圾邮件</div>
 			<div style="width: 100%; height: 50%; display: flex; flex-direction: column; align-items: center; justify-content: space-around;"> 
 				<div style="width: 100%; height: 45%; min-height: 70px; display: flex; align-items: center; justify-content: center;">
-					<div style="width: 70px; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+						<div style="width: 70px; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
 						<div class="TF_radio_box" style="width: 20px; height: 20px;">
 	 						<input name="one_choice" type="radio" id="choice_1" value="1">
 	 						<label for="choice_1"></label>							
@@ -46,6 +45,8 @@ template_4 = `
 					<p id="left">非常不符合</p>
 					<p>&ensp;<span id="right">非常符合</span>&ensp;</p>
 				</div>
+
+
 
 			</div>
 		</div>
