78444ed89205b98ca9d1ae4c416bc7f44016eca9
hzy <69162582@qq.com>
2019-03-07 00:44:33 +0800

    1

--- benefit/page_shampoo_task.js
+++ benefit/page_shampoo_task.js
@@ -2,7 +2,7 @@ page_shampoo_task =`
 	<div style="width: 100%; height: 100%; position: fixed; display: flex; flex-direction: column; align-items: center; justify-content: center;">
 		<div style="width: 70%; height: 25%; display: flex; align-items: center; justify-content: flex-start;"><!-- 任务描述 -->
 			<div style="width: 200px; height: 100%; font-size: 35px; display: flex; justify-content: flex-end;"><strong>情景模拟：</strong></div>
-			<div style="width: 70%; height: 100%; font-size: 26px; line-height: 1.5;">你的洗发水刚好用完了，朋友安利你一个护发品牌ALERNA，你现在打算从ALERNA的洗发水产品里<strong>选择一款你偏好的洗发水</strong></div>
+			<div style="width: 70%; height: 100%; font-size: 26px; line-height: 1.5;">你的洗发水刚好用完了，朋友安利你一个护发品牌ALERNA，你现在打算从ALERNA的洗发水产品里<strong>选择一款你偏好的洗发水</strong>，不用考虑价格因素</div>
 		</div>
 		<div style="width: 60%; height: 20%; display: flex; align-items: center; justify-content: center; font-size: 18px; line-height: 1.5;">
 			注：ALTERNA一家专注于护发产品的生物科技公司，致力于采用高科技技术，结合创新配方，为顾客提供高品质的护发产品
